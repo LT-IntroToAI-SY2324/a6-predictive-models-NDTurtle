d46b644b6c39ce54ece4fe18376e90d441e36043
NDTurtle <ndstalley@cps.edu>
2023-12-08 10:27:28 -0600

    Finished part 4

--- part4-classification/a6_part4.py
+++ part4-classification/a6_part4.py
@@ -10,18 +10,38 @@ x = data[["Age", "EstimatedSalary", "Gender"]].values
 y = data["Purchased"].values
 
 # Step 1: Print the values for x and y
-
+print(x)
+print(y)
 # Step 2: Standardize the data using StandardScaler, 
-
+scaler = StandardScaler()
 # Step 3: Transform the data
-
+x = scaler.fit_transform(x)
 # Step 4: Split the data into training and testing data
-
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.2, random_state=42)
 # Step 5: Fit the data
 
 # Step 6: Create a LogsiticRegression object and fit the data
-
+logreg = linear_model.LogisticRegression()
+logreg.fit(xtrain, ytrain)
 # Step 7: Print the score to see the accuracy of the model
+accuracy = logreg.score(xtest, ytest)
+print("\nAccuracy of the model:", accuracy)
 
-# Step 8: Print out the actual ytest values and predicted y values
-# based on the xtest data
+# Step 8: Print out the actual ytest values and predicted y values based on the xtest data
+y_pred = logreg.predict(xtest)
+print("\nActual ytest values:")
+print(ytest)
+print("\nPredicted y values based on xtest:")
+print(y_pred)
+
+# 34 year old female data point
+new_data_point = [[34, 56000, 1]]  # Assuming 1 represents Female
+
+# Scaling it
+scaled_new_data_point = scaler.transform(new_data_point)
+
+# Make prediction using model
+prediction = logreg.predict(scaled_new_data_point)
+
+# Print the prediction
+print("Prediction:", prediction)
--- part4-classification/a6_part4_writeup.md
+++ part4-classification/a6_part4_writeup.md
@@ -5,10 +5,10 @@ After completing `a6_part4.py` answer the following questions
 ## Questions to answer
 
 1. Comment out the StandardScaler and re-run your test. How accurate is the model? Why is that?
-
+It becomes completely wrong, and the predicted y values are all 0. This is because the StandardScaler puts the measurements on a common scale, and removing it leads to problems with data interpretation and inaccurate predictions. 
 2. How accurate is the model with the StandardScaler? Is this model accurate enough for the given use case? Explain.
-
+It's fairly accurate, with an 89% accuracy level compared to 60% without the scaler. This is accurate enough for the use case, as predicting if someone will buy an SUV doesn't require 100% accuracy, just enough to help with sales or whatever a model like this would be used for.
 3. Looking at the predicted and actual results, how did the model do? Was there a pattern to the inputs that the model was incorrect about?
-
+It did well. When it was incorrect, it seemed to be when it was predicting that someone would buy an SUV, but rarely when someone wouldn't buy an SUV.
 4. Would a 34 year old Female who makes 56000 a year buy an SUV according to the model? Remember to scale the data before running it through the model.
-
+The model predicts NO
